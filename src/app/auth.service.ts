cba97857bd7a76b76fc3c68edcbef66ce4c93fcc
muzenjakkevin <muzenjak57@gmail.com>
2020-02-21 15:48:30 +0100

    changes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AdminFull } from '../app/models/admin-full.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,16 +8,27 @@ export class AuthService {
   log(msg:any){console.log(msg);}
 
   loggedUser:string;
-  checkIfLoggedIn(){
 
+  public admins:AdminFull[] = [{
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'john.doe@email.com',
+  password: '1234567890'
+  }, {
+  firstName: 'Kylie',
+  lastName: 'Johnson',
+  email: 'kylie.johnson@email.com',
+  password: '0987654321'
+  }];
+
+  checkIfLoggedIn(){
   }
 
   login(){
-
   }
 
   logout(){
-
   }
+
   constructor() { }
 }
---
+++ src/app/models/admin-login.model.ts
@@ -0,0 +1,7 @@
+export class AdminLogin {
+
+ constructor(
+ public email: string,
+ public password: string
+ ) { }
+}
